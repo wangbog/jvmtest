71965e4a726eaadce274cf95da1aad727eea8a57
王博 <wangbo@qiyi.com>
2018-09-04 15:19:56 +0800

    test branch

--- src/main/java/com/wangbo/test/IntegerCompare.java
+++ src/main/java/com/wangbo/test/IntegerCompare.java
@@ -2,7 +2,7 @@ package com.wangbo.test;
 
 public class IntegerCompare {
 
-    public static void main(String args[]) {
+    public static void main(String [] args) {
         Integer i1 = 100;
         Integer i2 = 100;
         System.out.println(i1==i2);
@@ -10,6 +10,7 @@ public class IntegerCompare {
         Integer i3 = 300;
         Integer i4 = 300;
         System.out.println(i3==i4);
+
     }
 
 }
